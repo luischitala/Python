ab382450326ff181ec646b35f4a073996c6d1845
Luis Chitala <lrchitala@gmail.com>
2019-09-23 14:56:43 -0500

    Union and intersection created

--- conjuntos.py
+++ conjuntos.py
@@ -1,15 +1,53 @@
-#obtener los sets del usuario, los llamaremos A y B
-#debemos hacer función de union
-#debemos hacer funcion de interseccion
+
 #debemos hacer funcion de diferencia
 #debemos hacer funcion de diferencia simetrica
+def union_conjuntos(conjunto_a,conjunto_b):
+    print()
+    print("La unión de A y B es: {}".format(conjunto_a.union(conjunto_b)))
+    print()
 
 
-print("Bienvenido a los Conjuntos")
-print("Introduce los elementos de los conjuntos, separados por espacios")
-print("Ejemplo: 1 3 4 5 6")
+def interseccion_conjuntos(conjunto_a,conjunto_b):
+    print()
+    print("La intersección de A y B es: {}".format(conjunto_a.intersection(conjunto_b)))
+    print()
 
-conjunto_a = set(input("Conjunto A: ").split())
-conjunto_b = set(input("Conjunto B: ").split())
-print(conjunto_a)
-print(conjunto_b)
+
+def ver_instrucciones():
+    print("Operaciones que puedes realizar: ")
+    print("1 - Union")
+    print("2 - Intersección")
+    print("3 - Diferencia")
+    print("4 - Diferencia simétrica")
+    print("5 - Ver instrucciones")
+    print("6 - Salir")
+
+
+def calculadora_conjuntos():
+    print("Bienvenido a los Conjuntos")
+    print("Introduce los elementos de los conjuntos, separados por espacios")
+    print("Ejemplo: 1 3 4 5 6")
+
+    conjunto_a = set(input("Conjunto A: ").split())
+    conjunto_b = set(input("Conjunto B: ").split())
+    ver_instrucciones()
+
+
+    while True:
+        operacion = int(input(":"))
+        if operacion == 1:
+            union_conjuntos(conjunto_a,conjunto_b)
+        elif operacion == 2:
+            interseccion_conjuntos(conjunto_a,conjunto_b)
+        elif operacion == 3:
+            print("Diferencia")
+        elif operacion == 4:
+            print("Diferencia simétrica")
+        elif operacion == 5:
+            ver_instrucciones()
+        elif operacion == 6:
+            break
+        else:
+            print("No reconozco esa operación, Intenta de nuevo primo")
+
+calculadora_conjuntos()
