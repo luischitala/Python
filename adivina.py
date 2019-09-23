20f525c6acc4fa91ad1aa706da5ff799559a3da3
Luis Chitala <lrchitala@gmail.com>
2019-09-23 11:11:53 -0500

    To do list added

--- adivina.py
+++ adivina.py
@@ -1,5 +1,10 @@
+#Dar 5 vidas al usuario
+#Dar pistas, si el numero es mayor o menor 
+#Queremos poner la opción de volver a jugar generando número diferente
+
+
 import random
-numero_aleatorio = random.radint(1,10)
+numero_aleatorio = random.randint(1,10)
 
 print("Bienvenido a Adivina el número")
 print("Estoy pensando en un número del 1 al 10")
