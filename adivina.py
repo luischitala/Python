124a8bff67249c5235d3c60a025a444a0a9fd940
Luis Chitala <lrchitala@gmail.com>
2019-09-23 11:20:16 -0500

    Lives function added

--- adivina.py
+++ adivina.py
@@ -4,21 +4,27 @@
 
 
 import random
-numero_aleatorio = random.randint(1,10)
-
-print("Bienvenido a Adivina el número")
-print("Estoy pensando en un número del 1 al 10")
-print("Adivina cual es")
-while True:
-    adivinanza = int(input("El número es: "))
-    if adivinanza == numero_aleatorio:
-        print("Adivinaste!")
-        break
-    else:
-        if numero_aleatorio > adivinanza:
-            print("Fallaste, mi número es mayor")
+def jugar():
+    intentos = 0
+    numero_aleatorio = random.randint(1,10)
+    print("Bienvenido a Adivina el número")
+    print("Estoy pensando en un número del 1 al 10")
+    print("Adivina cual es")
+    print("Solamente tienes 5 intentos")
+    while intentos < 5:
+        adivinanza = int(input("El número es: "))
+        if adivinanza == numero_aleatorio:
+            print("Adivinaste!")
+            break
         else:
-            print("Fallaste, mi número es menor")
+            intentos += 1
+            if numero_aleatorio > adivinanza:
+                print("Fallaste, mi número es mayor")
+            else:
+                print("Fallaste, mi número es menor")
 
-        print("Fallaste, inténtalo de nuevo")
+            print("Fallaste, inténtalo de nuevo")
+print("Se te acabron los intentos")
 print("Gracias por jugar")
+
+jugar()
