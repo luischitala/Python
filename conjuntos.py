e877b84b80cdd3fc2445d524130fad3f5ccf4df7
Luis Chitala <lrchitala@gmail.com>
2019-09-23 12:28:22 -0500

    Set excercise created

---
+++ conjuntos.py
@@ -0,0 +1,15 @@
+#obtener los sets del usuario, los llamaremos A y B
+#debemos hacer función de union
+#debemos hacer funcion de interseccion
+#debemos hacer funcion de diferencia
+#debemos hacer funcion de diferencia simetrica
+
+
+print("Bienvenido a los Conjuntos")
+print("Introduce los elementos de los conjuntos, separados por espacios")
+print("Ejemplo: 1 3 4 5 6")
+
+conjunto_a = set(input("Conjunto A: ").split())
+conjunto_b = set(input("Conjunto B: ").split())
+print(conjunto_a)
+print(conjunto_b)
