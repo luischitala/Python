24662620670a9d6a0d6b21d5c31554411c77707b
Luis Chitala <lrchitala@gmail.com>
2019-09-26 22:41:24 -0500

    Basic write and read program

--- main1.py
+++ main1.py
@@ -1,4 +1,23 @@
 
+print("Bienvenidos al sistema de inventarios ")
+
+def menu():
+    print("1 - Mirar artículos")
+    print("2 - Agregar artículos")
+    print("3 - Salir")
+
+def opcion():  
+    while True:
+        operacion = input("Ingresa la opción deseada: ")
+        if operacion == "1":
+            mirar_articulo()
+        elif operacion == "2":
+            agregar_articulo(input("Articulo que quieres agregar: "))
+        elif operacion == "3":
+            break
+        else:
+            print("Ingrese una opción válida")
+
 
 def agregar_articulo(articulo):
     archivo_lista = open("lista.txt","a")
@@ -6,4 +25,15 @@ def agregar_articulo(articulo):
     archivo_lista.write("{}\n".format(articulo))
     archivo_lista.close()
 
-agregar_articulo(input("Artículo que quieres agregar: "))
+
+def mirar_articulo():
+    archivo_lista = open("lista.txt","r")
+    contenido = archivo_lista.read()
+    archivo_lista.close()
+    print(contenido)
+
+
+
+menu()
+opcion()
+
