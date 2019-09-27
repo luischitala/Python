32d2af7840b89bfc718031453ece8a9bf1286ee8
Luis Chitala <lrchitala@gmail.com>
2019-09-26 20:22:14 -0500

    New functions added and zone string introduced

--- reloj_mundo.py
+++ reloj_mundo.py
@@ -11,22 +11,36 @@ def mostrar_menu():
     print("5 Ver las instrucciones nuevamente")
     print("6 Para salir")
 
-def ver_hora():
-    time_zone = datetime.timezone(datetime.timedelta(hours=-6))
-    hora_actual = datetime.datetime.now(time_zone).time()
-    print(hora_actual)
+def ver_hora(zona_horaria):
+    if zona_horaria == -6:
+        zona = "Tiempo Central"
+    elif zona_horaria == -8:
+        zona = "Tiempo del Pacífico"
+    else:
+        zona = "Tiempo del Este"
+
+    formato = "%H:%M:%S"
+    zona_horaria = datetime.timezone(datetime.timedelta(hours=zona_horaria))
+    hora_actual = datetime.datetime.now(zona_horaria).time()
+    hora_formateada = hora_actual.strftime(formato)
+    print("La hora exacta en {} es: {}".format(zona,hora_formateada))
+def ver_fecha_hora():
+    formato = "%B, %d, %Y"
+    zona_horaria = datetime.timezone(datetime.timedelta(hours=-6))
+    fecha_actual = datetime.datetime.now(zona_horaria)
+    fecha_formateada = fecha_actual.strftime(formato)
+    print("La fecha es: {}".format(fecha_formateada))
 def ver_reloj():
     while True:
         operacion = input(": ")
-        
         if operacion == "1":
-            ver_hora()
+            ver_hora(-6)
         elif operacion == "2":
-            print(datetime.datetime.now())
+            ver_fecha_hora()
         elif operacion == "3":
-            print("Hora en Nueva York")
+            ver_hora(-5)
         elif operacion == "4":
-            print("Hora en San Francisco")
+            ver_hora(-8)
         elif operacion == "5":
             mostrar_menu()
         elif operacion == "6":
@@ -39,6 +53,7 @@ def ver_reloj():
             print()
 
             mostrar_menu()
+    print("Gracias por utilizar el reloj Mundial")
 
 mostrar_menu()
 ver_reloj()
